a6c872f45dca05ee4593522ad4f193146b361be6
vivek kumar verma <vivek.impdoc@gmail.com>
2026-05-07 10:19:37 +0530

    Setup GitHub Pages deployment with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Aarav-Kids-Learning-Game/',
   plugins: [
     react(),
     tailwindcss(),
